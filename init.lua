fe157131f03a9944f02e3b6dda8cf9661f749e53
james-francis-mt <james.francis@madetech.com>
2024-02-26 22:46:17 +0000

    part 2 setup finished

--- init.lua
+++ init.lua
@@ -1,11 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+        options = {
+            theme = 'dracula'
+        }
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
