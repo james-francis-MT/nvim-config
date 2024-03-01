8b77f1b7bc9cef8afcfcdc08efb60dd1f6f6bf41
james-francis-mt <james.francis@madetech.com>
2024-03-01 22:49:08 +0000

    format plugin files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,18 +4,7 @@ return {
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"elixir",
-				"heex",
-				"javascript",
-				"html",
-				"python",
-			},
+            auto_install = true,
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
