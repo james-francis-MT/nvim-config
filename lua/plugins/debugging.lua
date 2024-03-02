ad99a0fb3b976284fad24f6c736c57730bbb74f1
james-francis-mt <james.francis@madetech.com>
2024-03-02 13:47:41 +0000

    update plugins

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,30 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "mfussenegger/nvim-dap-python",
+    },
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
+
+        require("dap-python").setup(tostring(os.getenv("VIRTUAL_ENV")) .. "/bin/python")
+        require("dapui").setup()
+
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+
+        vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+        vim.keymap.set("n", "<leader>dc", dap.continue, {})
+    end,
+}
