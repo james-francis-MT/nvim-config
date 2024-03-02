666aefaf5116271865d3f99fca9090d0418ac153
james-francis-mt <james.francis@madetech.com>
2024-03-02 00:16:21 +0000

    add extra plugins

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,5 +1,31 @@
 return {
     {
+        "echasnovski/mini.pairs",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "<leader>up",
+                function()
+                    local Util = require("lazy.core.util")
+                    vim.g.minipairs_disable = not vim.g.minipairs_disable
+                    if vim.g.minipairs_disable then
+                        Util.warn("Disabled auto pairs", { title = "Option" })
+                    else
+                        Util.info("Enabled auto pairs", { title = "Option" })
+                    end
+                end,
+                desc = "Toggle auto pairs",
+            },
+        },
+    },
+    {
+        "ray-x/lsp_signature.nvim",
+        config = function()
+            require("lsp_signature").setup({})
+        end,
+    },
+    {
         "hrsh7th/cmp-nvim-lsp",
     },
     {
@@ -26,11 +52,21 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
+                    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
                     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<S-CR>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    }),
+                    ["<C-CR>"] = function(fallback)
+                        cmp.abort()
+                        fallback()
+                    end,
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
