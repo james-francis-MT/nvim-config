bfcbf107ac9dd09cd19cd4cd6c1495400c64de15
james-francis-mt <james.francis@madetech.com>
2024-05-28 15:23:35 +0100

    add c formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,23 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.black.with({
-                    prefer_local = tostring(os.getenv("VIRTUAL_ENV")) .. "/bin/black"
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.black.with({
+                    prefer_local = tostring(os.getenv("VIRTUAL_ENV")) .. "/bin/black",
                 }),
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop,
-				null_ls.builtins.diagnostics.ltrs,
-			},
-		})
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.diagnostics.rubocop,
+                null_ls.builtins.formatting.rubocop,
+                null_ls.builtins.diagnostics.ltrs,
+                null_ls.builtins.diagnostics.cppcheck,
+            },
+        })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-        vim.keymap.set('v', '<Leader>gf', vim.lsp.buf.format, {})
-	end,
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        vim.keymap.set("v", "<Leader>gf", vim.lsp.buf.format, {})
+    end,
 }
