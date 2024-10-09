81df3ebc741f8c402d1b33cf0a268d280d50eff1
James Francis <james.francis@madetech.com>
2024-10-09 09:59:05 +0100

    remove duplicate keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -22,5 +22,3 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- delete without storing in buffer
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
