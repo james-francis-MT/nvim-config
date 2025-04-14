29a8b0d4060cbedcaca842b069b582090f222582
james-francis-mt <james.francis@madetech.com>
2025-04-14 18:16:33 +0100

    do not highlight semantictokens

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -24,11 +24,10 @@ return {
       },
     },
     config = function()
-      --[[
       require('lspconfig').util.default_config.on_init = function(client, _)
         client.server_capabilities.semanticTokensProvider = nil
       end
-      ]]
+
       require("mason").setup()
       require("mason-lspconfig").setup()
 
